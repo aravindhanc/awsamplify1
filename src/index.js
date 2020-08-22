ac9d15932cd333b344cd7a3919c29bd13c0728c6
Aravindhan Chandrasekar <aravindhan.c@gmail.com>
2020-08-22 14:17:44 +1000

    added authentication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+Amplify.configure(config);
 ReactDOM.render(
   <React.StrictMode>
     <App />
